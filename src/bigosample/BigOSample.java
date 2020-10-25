95e0d5949d30afca76370e5d091b41102254232e
poonynh_ <poon.roon3@mail.kmutt.ac.th>
2020-10-26 00:37:58 +0700

    [ADD] insertionsort with running time test

--- src/bigosample/BigOSample.java
+++ src/bigosample/BigOSample.java
@@ -74,6 +74,33 @@ public class BigOSample {
         cal = end - begin;
         System.out.println("TIME FOR BUBBLE SORT : " + cal);
     }
+
+    public static void runInsertionSort(){
+        Integer[] a1 = new Integer[N];
+        Integer[] a2 = new Integer[N];
+        Integer[] a3 = new Integer[N];
+        Integer[] a4 = new Integer[N];
+        initial(a1);
+        System.arraycopy(a1, 0, a2, 0, a1.length);
+        System.arraycopy(a1, 0, a3, 0, a1.length);
+        System.arraycopy(a1, 0, a4, 0, a1.length);
+
+        long begin;
+        long end;
+        long cal;
+
+        begin = System.currentTimeMillis();
+        Arrays.sort(a2);
+        end = System.currentTimeMillis();
+        cal = end - begin;
+        System.out.println("TIME FOR ARRAY SORT : " + cal);
+
+        begin = System.currentTimeMillis();
+        InsertionSort(a2);
+        end = System.currentTimeMillis();
+        cal = end - begin;
+        System.out.println("TIME FOR INSERT SORT : " + cal);
+    }
     
     static void selectionSort(Integer items[]) {
  
@@ -93,6 +120,24 @@ public class BigOSample {
         }
     
     }
+
+    static void insertionSort(Integer[] arr) 
+    { 
+        int n = arr.length; 
+        for (int i = 1; i < n; ++i) { 
+            int key = arr[i]; 
+            int j = i - 1; 
+  
+            /* Move elements of arr[0..i-1], that are 
+               greater than key, to one position ahead 
+               of their current position */
+            while (j >= 0 && arr[j] > key) { 
+                arr[j + 1] = arr[j]; 
+                j = j - 1; 
+            } 
+            arr[j + 1] = key; 
+        } 
+    } 
 
     static void bubbleSort(Integer[] arr) {
         int n = arr.length;
